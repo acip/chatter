5f12e984b7a68d647b2b801fe314b5bfd2d3bfdf
Ciprian Amariei <ciprian.amariei@gmail.com>
2019-04-03 09:23:49 +0000

    custom user relation keys

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -21,6 +21,6 @@ class Post extends Model
 
     public function user()
     {
-        return $this->belongsTo(config('chatter.user.namespace'));
+        return $this->belongsTo(config('chatter.user.namespace'), 'user_id', 'user_id');
     }
 }
